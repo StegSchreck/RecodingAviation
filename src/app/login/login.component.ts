f7965066fb68d0d0b1bceaf2bc78a9d92e2f39b3
Matthias Klebe <matthiasklebe@gmail.com>
2017-06-17 01:56:24 +0200

    Removed firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-login',
